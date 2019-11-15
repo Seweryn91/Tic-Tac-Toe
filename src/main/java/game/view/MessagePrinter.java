011a3d38d512ea2dd66d7f44d9fb97b4ed11f4b2
Seweryn91 <s.talaj@gmail.com>
2019-11-15 14:47:03 +0100

    Implement methods printVictory and printDraw

--- src/main/java/game/view/MessagePrinter.java
+++ src/main/java/game/view/MessagePrinter.java
@@ -1,4 +1,14 @@
 package game.view;
 
+import game.model.Value;
+
 public class MessagePrinter {
+
+    public void printVictory(Value value) {
+        System.out.println("Player " + value + " wins!");
+    }
+
+    public void printDraw() {
+        System.out.println("This round was draw!");
+    }
 }
