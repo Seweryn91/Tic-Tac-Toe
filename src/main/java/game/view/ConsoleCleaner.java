255333623f13cbaeb81991aded0d9d528e4ecf08
Seweryn91 <s.talaj@gmail.com>
2019-11-15 15:04:57 +0100

    Change method clearScreen to non-static

--- src/main/java/game/view/ConsoleCleaner.java
+++ src/main/java/game/view/ConsoleCleaner.java
@@ -2,7 +2,7 @@ package game.view;
 
 public class ConsoleCleaner {
 
-    public static void clearScreen() {
+    public void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
