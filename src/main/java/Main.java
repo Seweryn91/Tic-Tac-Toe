5073614420c9561196ef0218f25d8c60115ec468
Seweryn91 <s.talaj@gmail.com>
2019-11-15 15:05:20 +0100

    Refactor class Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,9 @@
-import game.controller.FieldCheckerImpl;
-import game.model.Board;
-import game.view.BoardPrinter;
-import game.model.PatternChecker;
-import game.model.Turn;
+import game.model.Runner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Board board = new Board();
-        BoardPrinter boardPrinter = new BoardPrinter(board);
-        FieldCheckerImpl fieldChecker = new FieldCheckerImpl();
-        PatternChecker pattenChecker = new PatternChecker(board, fieldChecker);
-        boardPrinter.printBoard();
-        Turn turn = new Turn(board, pattenChecker);
-        boolean end = turn.isGameWon();
-        while (!end) {
-            turn.playTurn();
-            boardPrinter.printBoard();
-            //end = turn.isGameWon();
-            turn.playBotTurn();
-            boardPrinter.printBoard();
-            end = turn.isGameWon();
-        }
+        Runner runner = new Runner();
+        runner.run();
     }
 }
